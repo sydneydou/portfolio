6218b015668f2d3325422994c41ed9c8cc925c65
Sydney Douglas <sydneydouglas@MacBook-Pro.hitronhub.home>
2020-05-12 16:57:27 -0700

    parallax images working

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import "./styles.css";
+import { Parallax } from "react-parallax";
 
-function About(){
-   
-    
-        return (  
-            <div className="about-container" id="about">
-                <h1 className="title">About Me</h1>
-                <div className="about-flex">
-                    <div className="headshot-container">
-                        <img  className="headshot" src={require('../../assets/images/headshot.jpg')}></img>
-                    </div>
-                    <div className="description-container">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                    </div>
-                </div>
-            </div>
-        );
-    
+function About() {
+  return (
+    <div className="about-container" id="about">
+      <h1 className="title">About Me</h1>
+      <div className="about-flex">
+        <div className="headshot-container">
+          <img
+            className="headshot"
+            src={require("../../assets/images/headshot.jpg")}
+          ></img>
+        </div>
+        <div className="description-container">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+        </div>
+      </div>
+      <Parallax
+        blur={0}
+        bgImage={require("../../assets/images/leaf-banner.webp")}
+        bgImageAlt="banner image"
+        strength={400}
+      >
+        <div className="leaf-banner"></div>
+      </Parallax>
+    </div>
+  );
 }
- 
+
 export default About;
--- src/pages/FrontPage/index.js
+++ src/pages/FrontPage/index.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import './styles.css';
-import Header from "../../components/Header"
-import { Parallax } from 'react-scroll-parallax';
+import React from "react";
+import "./styles.css";
+import Header from "../../components/Header";
+import { Parallax } from "react-parallax";
 
 function FrontPage() {
   return (
-    <div className="img">
-      <Parallax>
-      <Header />
-      <div className = "front-page">
-        <div className="title-name">
+    <Parallax
+      blur={0}
+      bgImage={require("../../assets/images/head.webp")}
+      bgImageAlt="banner image"
+      strength={400}
+    >
+      <div className="img">
+        <Header />
+        <div className="front-page">
+          <div className="title-name">
             <h1>Hi, I'm Sydney!</h1>
             <h2>Front End Developer. Professional Problem Solver.</h2>
+          </div>
         </div>
-       
-        </div>
-        </Parallax>
-    </div>
+      </div>
+    </Parallax>
   );
 }
 
